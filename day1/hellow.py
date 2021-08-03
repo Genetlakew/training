2c41f0e25b1d3d8efa8f486fe4801552f9ed41e3
Asabeneh <asabeneh@gmail.com>
2021-08-03 21:38:52 +0300

    some scripts has been added

--- day1/hellow.py
+++ day1/hellow.py
@@ -3,3 +3,11 @@ print ('i am from ethiopia')
 print (99 + 1)
 print (3 * 3 * 3)
 print (3 ** 3)
+
+# Let's add some more code here
+
+first_name = 'Genet'
+last_name = 'Lakew'
+country = 'Ethiopia'
+full_name = first_name + ' ' + last_name
+print(full_name, country)
